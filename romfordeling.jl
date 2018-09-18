271417eb97340caee80685a06fa17e28a6485b1b
Mathias Bynke <mathias@bynke.no>
2018-09-18 20:58:38 +0200

    Lagd hjelpevariabler og teller antall ønsker

--- romfordeling.jl
+++ romfordeling.jl
@@ -13,26 +13,38 @@ rom = [2, 2, 3]
 
 # Fylle ønskematrisen
 ønsker[1, 2] = 1
+ønsker[3, 4] = 1
+ønsker[2, 3] = 1
 
 m = Model(solver = CbcSolver())
 
-# rf[r, d]: Deltaker d bor på rom r. (RomFordeling)
-@variable(m, rf[1:length(rom), 1:antalldeltakere], Bin)
-# bsr[i, j, r]: Deltaker i og deltaker j bor sammen på rom r.
-@variable(m, bsr[1:antalldeltakere, 1:antalldeltakere, 1:length(rom)], Bin)
-#
-
-# Dummymålfunksjon: Plassere flest mulig personer på rom
-@objective(m, Max, sum([rf[r, i] * rf[r, j] for r in 1:length(rom), i in 1:antalldeltakere, j in 1:antalldeltakere if ønsker[i,j] == 1]))
+# bori[d, r]: Deltaker d bor på rom r. (RomFordeling)
+@variable(m, bori[1:antalldeltakere, 1:length(rom)], Bin)
 
 # Hver person bor på ett rom.
 for deltakernummer in 1:antalldeltakere
-    @constraint(m, sum(rf[:, deltakernummer]) == 1)
+    @constraint(m, sum(bori[deltakernummer, :]) == 1)
 end
 # Antall personer på et rom må ikke være høyere enn rommet sin kapasitet.
 for romnummer in 1:length(rom)
-    @constraint(m, sum(rf[romnummer, :]) <= rom[romnummer])
+    @constraint(m, sum(bori[:, romnummer]) <= rom[romnummer])
 end
+
+#HJELPEVARIABLER
+# bsr[i, j, r]: Deltaker i og deltaker j bor sammen på rom r.
+@variable(m, bsr[1:antalldeltakere, 1:antalldeltakere, 1:length(rom)], Bin)
+for i in 1:antalldeltakere
+    for j in 1:antalldeltakere
+        for r in 1:length(rom)
+            @constraint(m, bsr[i, j, r] <= bori[i, r])
+            @constraint(m, bsr[i, j, r] <= bori[j, r])
+            @constraint(m, bsr[i, j, r] >= bori[i, r] + bori[j,r] - 1)
+        end
+    end
+end
+
+# Dummymålfunksjon: Plassere flest mulig personer på rom
+@objective(m, Max, sum([sum(bsr[i, j, :]) for r in 1:length(rom), i in 1:antalldeltakere, j in 1:antalldeltakere if ønsker[i,j] == 1]))
 
 println(m)
 
@@ -41,4 +53,4 @@ status = solve(m)
 println(status)
 
 println("Målverdi: ", getobjectivevalue(m))
-rf = getvalue(rf)
+bori = getvalue(bori)

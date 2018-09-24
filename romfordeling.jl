e033e4b10b97782b2f415bd6b689dd56f3782934
Mathias Bynke <mathias@bynke.no>
2018-09-24 21:05:14 +0200

    Fungerende fiks

--- romfordeling.jl
+++ romfordeling.jl
@@ -17,6 +17,7 @@ rom = [2, 2, 3, 5]
 ønsker[1, 4] = 1
 ønsker[1, 5] = 1
 ønsker[1, 6] = 1
+ønsker[2, 4] = 1
 
 # MODELLEN
 m = Model(solver = CbcSolver())
@@ -36,7 +37,7 @@ for romnummer in 1:length(rom)
 end
 
 # HJELPEVARIABLER
-# bsr[i, j, r]: Deltaker i og deltaker j bor sammen på rom r.
+# bsr[i, j, r]: Deltaker i og deltaker j Bor Sammen på Rom r.
 @variable(m, bsr[1:antalldeltakere, 1:antalldeltakere, 1:length(rom)], Bin)
 for i in 1:antalldeltakere
     for j in 1:antalldeltakere
@@ -51,25 +52,17 @@ end
 # aoø[i]: Antall Oppfylte Ønsker for deltaker nr. i.
 @variable(m, aoø[1:antalldeltakere], Int)
 for i in 1:antalldeltakere
-    @info "Punkt 1"
-    filter = []
-    @info "Punkt 2"
-    for j in 1:antalldeltakere
-        @info "Punkt 3"
-        if ønsker[i,j] == 1
-            @info "Punkt 4"
-            push!(filter, j)
-        end
-    end
-    @info "Punkt 5" filter
-    @constraint(m, aoø[i] == sum([sum(bsr[i, k, :]) for k in filter]))
-    # @constraint(m, aoø[i] == sum([sum(bsr[i, j, :]) for j in 1:antalldeltakere if ønsker[i,j] == 1])) # Fungerer ikke!
+    # Lager en liste med en binær verdi (1 eller 0) for hvert av i sine ønsker.
+    # Lagrer listen midlertidig før vi summerer og finner antall oppfylte ønsker
+    # for å omgå at sum() ikke vil summere en tom liste (av typen Array{Any, 1}).
+    tmp = [sum(bsr[i, j, :]) for j in 1:antalldeltakere if ønsker[i,j] == 1]
+    @constraint(m, aoø[i] == sum(isempty(tmp) ? [0] : tmp))
 end
 
 
 # MÅLFUNKSJON
-# Dummymålfunksjon: Plassere flest mulig personer på rom
-@objective(m, Max, sum([sum(bsr[i, j, :]) for i in 1:antalldeltakere, j in 1:antalldeltakere if ønsker[i,j] == 1]))
+# Maksimer antall oppfylte ønsker.
+@objective(m, Max, sum(aoø))
 
 # println(m)
 
@@ -79,4 +72,10 @@ println(status)
 
 println("Målverdi: ", getobjectivevalue(m))
 bori = getvalue(bori)
+println()
+println("Romplassering. Rader: rom. Kolonner: Deltakere.")
 display(bori)
+aoø = getvalue(aoø)
+println()
+println("Antall ønsker oppfylt for hver deltaker:")
+display(aoø)
